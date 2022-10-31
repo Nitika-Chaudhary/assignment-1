32c252afac9c5c584eda47d1ae4a0bd6b326967f
inntelligent <115881318+inntelligent@users.noreply.github.com>
2022-10-31 13:24:54 +0100

    Add files via upload

---
+++ assignment2.java
@@ -0,0 +1,27 @@
+package pattern;
+
+public class assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<4;j++)
+				
+				{
+				System.out.print(a);
+				
+			}
+		
+		System.out.println();
+		a++;
+		}
+	}
+
+}
+
+							
+
+	
---
+++ assignment5.java
@@ -0,0 +1,25 @@
+package pattern;
+
+public class assignment5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=20;
+for (int i=0;i<n;i++)
+{
+	for(int j=0; j<n; j++)
+	{
+		if(i==0 || i+j<=(n-1)/2 || j==0 || i==n-1 || i-j>=(n-1)/2)
+		{
+			System.out.print("*");
+		}
+		else
+		{
+			System.out.print(" ");
+		}
+	}
+	System.out.println();	
+}
+	}
+
+}
